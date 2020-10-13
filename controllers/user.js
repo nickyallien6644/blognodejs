485e9823f4740a919e61436709766fcef1cf4885
nickyallien6644 <62051422+nickyallien6644@users.noreply.github.com>
2020-10-14 01:16:23 +0700

    login page

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,21 @@ const Image = require('../models/image');
 exports.getLogin = (req,res,next) => {
     res.render('post/login', {
         pageTitle: 'Login',
-        url: '/login'
+        url: '/login',
     });
 }
+
+exports.login = (req,res,next) => {
+    const email = req.body.txtEmail;
+    const password = req.body.txtPass;
+
+    User.findOne({where: {email: email, password: password}})
+        .then(user => {
+            res.render('post/successful', {
+                pageTitle: 'Successful',
+                url: '/successful',
+                user: user,
+            });
+        })
+        .catch(err => console.log(err));
+};
