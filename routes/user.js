0cd0199cfec06374f213ee0cf3491647c7e44fdd
nickyallien6644 <62051422+nickyallien6644@users.noreply.github.com>
2020-10-14 03:31:16 +0700

    add cookie va trang dang ki nhung bi loi

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/user');
 
 router.get('/login', userController.getLogin);
+router.get('/registration', userController.getRegistration);
+
 router.post('/login', userController.login);
+
 
 module.exports = router;
